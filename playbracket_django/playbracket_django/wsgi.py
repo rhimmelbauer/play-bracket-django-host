c965085413eb3a21c74d86da8ac7b7096217a6b8
Roberto Himmelbauer <robertoh89@gmail.com>
2024-10-24 11:04:56 -0700

    initial django project

---
+++ playbracket_django/playbracket_django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for playbracket_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playbracket_django.settings')
+
+app = get_wsgi_application()
